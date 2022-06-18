ba12c598c863e1ade24b12f7fc5e2bdeab92846c
vrindavan sanap <vrindavansanap@gmail.com>
2022-06-18 07:35:00 +0530

    first publis

--- my-website/src/App.tsx
+++ my-website/src/App.tsx
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='title'>
+        <h1>QuaternionCS</h1>
+      </div>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/0/0c/FieldsMedalFront.jpg" alt='logo' className='logo' />
+      <div className='quote'>
+        The mathematician plays a game in which he himself invents the rules while the physicist plays a game in which the rules are provided by nature, but as time goes on it becomes increasingly evident that the rules which the mathematician finds interesting are the same as those which nature has chosen.
 
+      </div>
     </div>
   );
 }
